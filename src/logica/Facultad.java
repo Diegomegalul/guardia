8cc4b4231c98664bf9fca8496fa80c7f41fb1122
Diegomegalul <dcanalescalvos@gmail.com>
2025-06-08 17:05:11 -0400

    Arreglo la Facultad

--- src/logica/Facultad.java
+++ src/logica/Facultad.java
@@ -1,6 +1,29 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import logica.Persona;
+
 public class Facultad {
+	//Atributos
+	private static final List<Persona>personas = new ArrayList<>();
+	
+	//Constructor
+	public Facultad(){
 
-
+	}
+	//Getters y setters
+	public List<Persona> getPersonas(){
+		return Collections.unmodifiableList(personas);
+	}
+	//Metodos
+	public void agregarPersona(Persona persona){
+		personas.add(persona);
+	}
+	
+	public void eliminarPersona(Persona persona){
+		personas.remove(persona);
+	}
 }
